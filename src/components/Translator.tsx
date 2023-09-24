d4e309094009fafbbb2c1e8e656d9140fdf16e33
FreyzerFault <martinezalcantara29@gmail.com>
2023-09-24 18:46:29 +0200

    fix build

--- src/components/Translator.tsx
+++ src/components/Translator.tsx
@@ -1,46 +1,5 @@
-import { useReducer } from 'react'
 import '../styles/Translator.css'
-import { type AppState, type Action } from '../types.d'
 import { useTranslator } from '../hooks/useTranslator'
-
-const initialState: AppState = {
-  fromLanguage: 'auto',
-  toLanguage: 'en',
-  fromText: '',
-  result: '',
-  loading: false,
-}
-
-function reducer(state: AppState, action: Action): AppState {
-  const { type } = action
-
-  if (type === 'SWITCH_LANGUAGE') {
-    return {
-      ...state,
-      // SWITCH languages
-      fromLanguage: state.toLanguage,
-      toLanguage: state.fromLanguage,
-    }
-  }
-  if (type === 'SET_FROMLANGUAGE') {
-    return { ...state, fromLanguage: action.payload }
-  }
-
-  if (type === 'SET_TOLANGUAGE') {
-    return { ...state, toLanguage: action.payload }
-  }
-
-  if (type === 'SET_FROMTEXT') {
-    return { ...state, fromText: action.payload, loading: true, result: '' }
-  }
-
-  if (type === 'SET_RESULT') {
-    return { ...state, result: action.payload, loading: false }
-  }
-
-  console.warn('Reducer ha cogido una accion no conocida:', type)
-  return state
-}
 
 export function Translator() {
   const { state, switchLanguages, setFromLanguage } = useTranslator()
